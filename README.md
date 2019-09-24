c67f133f1bd71aa93dcaf27d4060a602c55cd9da
Aurélien Pupier <apupier@redhat.com>
2019-09-24 10:50:32 +0200

    Provide example based on Camel #1
    nota:
    - not all languages have syntax coloring, this is a limitation of
    CodeMirror
    - only Camel LSP provided. It means only Camel URI support. No other
    language support.
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-This is an example of integrating CodeMirror in the browser with a remotely-hosted
-language server, which is connected over web socket.
+# Example of CodeMirror integration with an Apache Camel Language Server
+
+This is an example of integrating CodeMirror in the browser with an Apache Camel language server, which is connected over web socket.
+
+# How to test
+
+- Launch Camel Language server with `--websocket` parameter locally
+- Launch `npm install`
+- Open `index.html` in a browser
+- Enjoy!
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import CodeMirror from 'codemirror';
+import 'codemirror/mode/groovy/groovy';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/htmlmixed/htmlmixed';
-import 'codemirror/mode/css/css';
-import 'codemirror/mode/go/go';
-import 'codemirror/mode/python/python';
+import 'codemirror/mode/properties/properties';
+import 'codemirror/mode/yaml/yaml';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/idea.css';
 // This addon is required to be installed by the caller
@@ -13,24 +13,116 @@ import 'codemirror/addon/hint/show-hint';
 import 'lsp-editor-adapter/lib/codemirror-lsp.css';
 import { LspWsConnection, CodeMirrorAdapter } from 'lsp-editor-adapter';
 
-let sample = '';
+let sampleXml = 
+`<camelContext id="example-context" xmlns="http://camel.apache.org/schema/blueprint">
+        <route id="direct-input">
+            <from id="_from1" uri="file:work/input"/>
+            <to id="_processing" uri="direct:processing"/>
+        </route>
+    </camelContext>
+`;
+
+let sampleJava = 
+`import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class MyRouteBuilder extends RouteBuilder {
+
+    /**
+     * Let's configure the Camel routing rules using Java code...
+     */
+    public void configure() {
+
+        // here is a sample which processes the input files
+        // (leaving them in place - see the 'noop' flag)
+        // then performs content based routing on the message using XPath
+        from("file:src/data?noop=true&antExclude=aa&antFilterCaseSensitive=true&runLoggingLevel=OFF")
+            .choice()
+                .when(xpath("/person/city = 'London'"))
+                    .to("file:target/messages/uk")
+                .otherwise()
+                    .to("file:target/messages/others");
+    }
+
+}
+`;
+
+let sampleKotlin = 
+`from("timer:kotlin?period=1s")
+  .routeId("kotlin")
+  .setBody()
+    .constant("Hello Camel K!")
+  .process().message {
+    it.headers["RandomValue"] = rnd.nextInt()
+    }
+  .to("log:info?showAll=true&multiline=true")
+`;
+
+let sampleJavascript = 
+`from('timer:js?period=1s')
+  .routeId('js')
+  .setBody()
+    .simple('Hello Camel K from CodeMirror')
+  .to('log:info?multiline=true')`;
+let sampleGroovy = 
+`from('direct:greeting-api')
+  .to('log:api?showAll=true&multiline=true') 
+  .setBody()
+    .simple('Hello from CodeMirror')`;
+let sampleYaml = 
+`- id: "rest"
+  group: "routes"
+  rest:
+    verb: "post"
+    uri: "/api/route"
+    accepts: "text/plain"
+    binding-mode: "off"
+    steps:
+      - convert-body:
+          type: "java.lang.String"
+      - to:
+          uri: "log:in"`;
+let sampleKafkaConnectProperties = 
+`name=CamelAWSS3SourceConnector
+connector.class=org.apache.camel.kafkaconnector.CamelSourceConnector
+key.converter=org.apache.kafka.connect.storage.StringConverter
+value.converter=org.apache.camel.kafkaconnector.converters.S3ObjectConverter
+camel.source.maxPollDuration=10000
+
+camel.source.kafka.topic=mytopic1
+
+camel.source.url=aws-s3://bucket?autocloseBody=false
+
+camel.component.aws-s3.configuration.access-key=<youraccesskey>
+camel.component.aws-s3.configuration.secret-key=<yoursecretkey>
+camel.component.aws-s3.configuration.region=<yourregion>`;
 
 let modes = {
+  xml: 'xml',
+  java: 'java',
+  yaml: 'yaml',
+  kotlin: 'kotlin',
   javascript: 'javascript',
-  html: 'htmlmixed',
-  css: 'css',
+  properties: 'properties',
+  groovy: 'groovy'
 };
 
 let documents = {
-  javascript: 'file.js',
-  html: 'file.html',
-  css: 'file.css',
+  xml: 'camel.xml',
+  java: 'CamelRoute.java',
+  yaml: 'file.camelk.yaml',
+  kotlin: 'file.camelk.kts',
+  javascript: 'file.camelk.js',
+  properties: 'camelKafkaConnect.properties',
+  groovy: 'file.camelk.groovy'
 };
 
 let editor = CodeMirror(document.querySelector('.editor'), {
   theme: 'idea',
   lineNumbers: true,
-  value: sample,
+  value: sampleXml,
   gutters: ['CodeMirror-lsp'],
 });
 
@@ -51,19 +143,30 @@ function switchSources() {
 
   let value = document.querySelector('select').value.toLowerCase();
 
-  if (typeof modes[value] !== undefined) {
-    editor.setOption('mode', modes[value]);
+  editor.setOption('mode', modes[value]);
+  if(value === "xml") {
+    editor.setValue(sampleXml);
+  } else if(value === "java") {
+    editor.setValue(sampleJava);
+  } else if(value === "groovy") {
+    editor.setValue(sampleGroovy);
+  } else if(value === "javascript") {
+    editor.setValue(sampleJavascript);
+  } else if(value === "properties") {
+    editor.setValue(sampleKafkaConnectProperties);
+  } else if(value === "kotlin") {
+    editor.setValue(sampleKotlin);
+  } else if(value === "yaml") {
+    editor.setValue(sampleYaml);
   }
 
-  editor.setValue('');
-
   connection = new LspWsConnection({
-    serverUri: 'wss://lsp-server.wylie.su/' + value,
-    languageId: value,
+    serverUri: 'ws://localhost:8025/camel-language-server',
+    languageId: "Apache Camel",
     rootUri: 'file:///usr/src/app/sources',
     documentUri: 'file:///usr/src/app/sources/' + documents[value],
     documentText: () => editor.getValue(),
-  }).connect(new WebSocket('wss://lsp-server.wylie.su/' + value));
+  }).connect(new WebSocket('ws://localhost:8025/camel-language-server'));
 
   adapter = new CodeMirrorAdapter(connection, {
     quickSuggestionsDelay: 10,
